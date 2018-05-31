01564b00b8f2d39cddb5a1d46c83e79a21dbeba1
udaykodeboina <38686785+udaykodeboina@users.noreply.github.com>
2018-05-31 14:07:52 +0530

    Create mp3download.py

---
+++ mp3download.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {
+   'format': 'bestaudio/best',
+   'postprocessors': [{
+       'key': 'FFmpegExtractAudio',
+       'preferredcodec': 'mp3',
+       'preferredquality': '192',
+   }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+   ydl.download(['https://www.youtube.com/watch?v=41qgdwd3zAg&list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n'])
